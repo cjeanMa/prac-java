c2c1dfefac81b76cee410ce6ce63d86ece8f4b14
JeanMa <cjeanma009@gmail.com>
2020-01-16 11:54:39 -0500

    Ejemplo de manejo de paquetes en java

---
+++ EjemploPaquetes/src/main/java/EjemploPaquetes.java
@@ -0,0 +1,21 @@
+//import com.jmcatle.Utileria
+//import com.jmcastle.*;
+
+import static com.jmcastle.Utileria.imprimir;
+
+
+public class EjemploPaquetes {
+    public static void main(String[] args) {
+        //utilizando el nombre de la clase completamente calificado
+        //fully qualified name
+        
+        //com.jmcastle.Utileria.imprimir("Hola");
+        
+        
+        //haciendo uso de import
+        //Utileria.imprimir("Hola");
+        
+        //haciendo uso de import de tipo static
+        imprimir("Hola");
+    }
+}
